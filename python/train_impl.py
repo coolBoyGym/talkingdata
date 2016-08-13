6179ba9ac200b9f9c501b961233e507844355432
Gaoyimei <gaoyimeihome@gmail.com>
2016-08-13 12:40:28 +0800

    Add new arguments to gblinear and gbtree training process.

--- python/train_impl.py
+++ python/train_impl.py
@@ -73,7 +73,7 @@ def write_log(log_str):
         fout.write(log_str)
 
 
-def tune_gblinear(dtrain, dvalid, gblinear_alpha, gblinear_lambda, gblinear_lambda_bias, verbose_eval=True,
+def tune_gblinear(dtrain, dvalid, gblinear_alpha=0, gblinear_lambda=0, gblinear_lambda_bias=0, verbose_eval=True,
                   early_stopping_rounds=50, dtest=None):
     global BOOSTER, RANDOME_STATE
     num_boost_round = 1000
@@ -109,13 +109,14 @@ def tune_gblinear(dtrain, dvalid, gblinear_alpha, gblinear_lambda, gblinear_lamb
     return train_score, valid_score
 
 
-def train_gblinear(dtrain_complete, dtest, gblinear_alpha, gblinear_lambda, num_boost_round):
+def train_gblinear(dtrain_complete, dtest, gblinear_alpha, gblinear_lambda, gblinear_lambda_bias, num_boost_round):
     global BOOSTER, RANDOME_STATE
     params = {
         'booster': BOOSTER,
         'silent': 1,
         'num_class': 12,
         'lambda': gblinear_lambda,
+        'lambda_bias': gblinear_lambda_bias,
         'alpha': gblinear_alpha,
         'objective': 'multi:softprob',
         'seed': RANDOME_STATE,
@@ -132,8 +133,9 @@ def train_gblinear(dtrain_complete, dtest, gblinear_alpha, gblinear_lambda, num_
     make_submission(test_pred)
 
 
-def tune_gbtree(dtrain, dvalid, eta, max_depth, subsample, colsample_bytree, verbose_eval, early_stopping_rounds=50,
-                dtest=None):
+def tune_gbtree(dtrain, dvalid, eta, max_depth, subsample, colsample_bytree, gamma=0, min_child_weight=1,
+                max_delta_step=0, verbose_eval=False,
+                early_stopping_rounds=50, dtest=None):
     global BOOSTER, RANDOME_STATE
     num_boost_round = 2000
 
@@ -145,6 +147,9 @@ def tune_gbtree(dtrain, dvalid, eta, max_depth, subsample, colsample_bytree, ver
         "max_depth": max_depth,
         "subsample": subsample,
         "colsample_bytree": colsample_bytree,
+        "gamma":gamma,
+        "min_child_weight":min_child_weight,
+        "max_delta_step":max_delta_step,
         "objective": "multi:softprob",
         "seed": RANDOME_STATE,
         "eval_metric": "mlogloss",
